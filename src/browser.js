fd51364ada4219911b0e01a297f41e01b1f22275
introkun <introkun@gmail.com>
2021-02-13 22:37:13 +0300

    updated browser argument

--- src/browser.js
+++ src/browser.js
@@ -6,7 +6,7 @@ async function startBrowser(headless = true) {
         console.log("Opening the browser......")
         browser = await puppeteer.launch({
             headless: headless,
-            args: ["--disable-setuid-sandbox"]
+            args: ["--disable-setuid-sandbox", "--no-sandbox"]
         })
     } catch (err) {
         console.log("Could not create a browser instance => : ", err)
