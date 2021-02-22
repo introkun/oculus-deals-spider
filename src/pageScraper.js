6409660ca809021d3d4baf5dfc02faeb3671b61e
introkun <introkun@gmail.com>
2021-02-22 17:08:33 +0300

    fixed page scraping

--- src/pageScraper.js
+++ src/pageScraper.js
@@ -206,6 +206,7 @@ const scrapeAll = async (browser, mainUrl) => {
     items: [],
   };
 
+  console.log('Scraping main page sections contents...');
   console.log('Wait for the required DOM to be rendered');
   try {
     await page.waitForSelector('div.store-section-item');
@@ -218,6 +219,8 @@ const scrapeAll = async (browser, mainUrl) => {
   for (let index = 1; index < sectionsCount; index++) {
     await processSection(page, {index: index}, result, true);
   }
+
+  console.log('Scraping favourite sections...');
   await processSection(page,
       {url: 'https://www.oculus.com/experiences/quest/section/2228099660560866/'}, result);
   await processSection(page,
@@ -245,94 +248,99 @@ const scrapeAll = async (browser, mainUrl) => {
   await processSection(page,
       {url: 'https://www.oculus.com/experiences/quest/section/336976240361150/'}, result, true);
 
+  console.log('Scraping main page experiences & deals...');
   await scrapeMainPage(page, result);
 
-  console.log('Trying to find deals in \'Quest Picks\' section');
-  console.log('Wait for the required DOM to be rendered');
   try {
-    await page.waitForSelector('span.store-section-item-price-label__promo',
-        {timeout: 40000});
-  } catch (err) {
-    console.log('Can\'t find element');
-    return result;
+    console.log('Trying to find deals in the top carousel...');
+    console.log('Wait for the required DOM to be rendered');
+    try {
+      await page.waitForSelector('span.store-section-item-price-label__promo',
+          {timeout: 40000});
+    } catch (err) {
+      console.log('Can\'t find element');
+      return result;
+    }
+    console.log('Found needed element');
+
+    const selector = '.store-section-item-tag.store-section-item-tag__blue.' +
+      'store-section-item-price-label__promo';
+    const link = await page.$$eval(selector, (items) => {
+      const parentNode = items[0].parentNode.parentNode.parentNode.parentNode;
+      const link = parentNode.parentNode.parentNode.parentNode.href;
+
+      return link;
+    });
+
+    if (link == undefined || link.length == 0) {
+      console.log('Can\'t find game\'s link');
+      return result;
+    }
+
+    console.log(`Navigating to ${link}...`);
+    await page.goto(link);
+
+    console.log('Wait for the required DOM to be rendered');
+    await page.waitForSelector('div.app-description__title');
+    console.log('Found needed element');
+
+    const game = await page.$$eval('div.app__row', (items) => {
+      const parsePrice = (str) => {
+        const priceArray = /^(.*)\$(.*)/g.exec(str);
+        const priceCurrency = `${priceArray[1]}$`; const priceValue = +priceArray[2];
+        console.log(`priceArray ${priceArray}`);
+        console.log(`priceCurrency ${priceCurrency}`);
+        console.log(`priceValue ${priceValue}`);
+        const obj = {};
+        obj['currency'] = priceCurrency;
+        obj['value'] = priceValue;
+        return obj;
+      };
+
+      console.log('Found game block');
+      const game = {};
+      const item = items[0];
+      console.log('Looking for title');
+      game['name'] = item.querySelector('.app-description__title').innerText;
+
+      const discountSelector = '.app-purchase-price-discount-detail__promo-benefit';
+      const discountString = item.querySelectorAll(discountSelector)[0].innerText;
+      console.log(`dicsountString ${discountString}`);
+      const discountValue = /^-(.*)%$/g.exec(discountString)[1];
+      game['discountPercent'] = +discountValue;
+
+      const salePrice = item.querySelector('span.app-purchase-price > span').innerText;
+      const salePriceObj = parsePrice(salePrice);
+      console.log(`salePriceObj ${salePriceObj}`);
+      game['salePrice'] = salePriceObj.value;
+      game['salePriceCurrency'] = salePriceObj.currency;
+
+      const priceSelector = '.app-purchase-price-discount-detail__strikethrough-price > span';
+      const price = item.querySelector(priceSelector).innerText;
+      const priceObj = parsePrice(price);
+      console.log(`priceObj ${priceObj}`);
+      game['price'] = priceObj.value;
+      game['priceCurrency'] = priceObj.currency;
+
+      const now = Date.now();
+      const timer = item.querySelector('.store-item-countdown-timer__timer').innerText;
+      console.log(`timer ${timer}`);
+      const timeSplitted = timer.split(':');
+      const endsInMs = (Number(timeSplitted[0]) * 60 * 60 + Number(timeSplitted[1]) * 60 +
+                Number(timeSplitted[2])) * 1000;
+      console.log(`endsInMs ${endsInMs}`);
+      const endsUtcDate = new Date(now + endsInMs);
+      console.log(`endsUtcDate ${endsUtcDate}`);
+      game['endsUtc'] = endsUtcDate.toISOString();
+
+      return game;
+    });
+    game['url'] = link;
+
+    result.items.push(game);
+  } catch (e) {
+    console.log(`Can\'t find game in the carousel: ${e}`);
   }
-  console.log('Found needed element');
-
-  const selector = '.store-section-item-tag.store-section-item-tag__blue.' +
-    'store-section-item-price-label__promo';
-  const link = await page.$$eval(selector, (items) => {
-    const parentNode = items[0].parentNode.parentNode.parentNode.parentNode;
-    const link = parentNode.parentNode.parentNode.parentNode.href;
-
-    return link;
-  });
-
-  if (link == undefined || link.length == 0) {
-    console.log('Can\'t find game\'s link');
-    return result;
-  }
-
-  console.log(`Navigating to ${link}...`);
-  await page.goto(link);
-
-  console.log('Wait for the required DOM to be rendered');
-  await page.waitForSelector('div.app-description__title');
-  console.log('Found needed element');
-
-  const game = await page.$$eval('div.app__row', (items) => {
-    const parsePrice = (str) => {
-      const priceArray = /^(.*)\$(.*)/g.exec(str);
-      const priceCurrency = `${priceArray[1]}$`; const priceValue = +priceArray[2];
-      console.log(`priceArray ${priceArray}`);
-      console.log(`priceCurrency ${priceCurrency}`);
-      console.log(`priceValue ${priceValue}`);
-      const obj = {};
-      obj['currency'] = priceCurrency;
-      obj['value'] = priceValue;
-      return obj;
-    };
-
-    console.log('Found game block');
-    const game = {};
-    const item = items[0];
-    console.log('Looking for title');
-    game['name'] = item.querySelector('.app-description__title').innerText;
-
-    const discountSelector = '.app-purchase-price-discount-detail__promo-benefit';
-    const discountString = item.querySelectorAll(discountSelector)[0].innerText;
-    console.log(`dicsountString ${discountString}`);
-    const discountValue = /^-(.*)%$/g.exec(discountString)[1];
-    game['discountPercent'] = +discountValue;
-
-    const salePrice = item.querySelector('span.app-purchase-price > span').innerText;
-    const salePriceObj = parsePrice(salePrice);
-    console.log(`salePriceObj ${salePriceObj}`);
-    game['salePrice'] = salePriceObj.value;
-    game['salePriceCurrency'] = salePriceObj.currency;
-
-    const priceSelector = '.app-purchase-price-discount-detail__strikethrough-price > span';
-    const price = item.querySelector(priceSelector).innerText;
-    const priceObj = parsePrice(price);
-    console.log(`priceObj ${priceObj}`);
-    game['price'] = priceObj.value;
-    game['priceCurrency'] = priceObj.currency;
-
-    const now = Date.now();
-    const timer = item.querySelector('.store-item-countdown-timer__timer').innerText;
-    console.log(`timer ${timer}`);
-    const timeSplitted = timer.split(':');
-    const endsInMs = (Number(timeSplitted[0]) * 60 * 60 + Number(timeSplitted[1]) * 60 +
-              Number(timeSplitted[2])) * 1000;
-    console.log(`endsInMs ${endsInMs}`);
-    const endsUtcDate = new Date(now + endsInMs);
-    console.log(`endsUtcDate ${endsUtcDate}`);
-    game['endsUtc'] = endsUtcDate.toISOString();
-
-    return game;
-  });
-  game['url'] = link;
-
-  result.items.push(game);
 
   return result;
 };
